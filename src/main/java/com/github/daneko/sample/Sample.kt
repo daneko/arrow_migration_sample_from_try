4710b3fabe9926c4b44a54e7ae35e7f51bcbaf6d
daneko <3a3k.web+github@gmail.com>
2019-02-21 21:29:16 +0900

    use either

--- src/main/java/com/github/daneko/sample/Sample.kt
+++ src/main/java/com/github/daneko/sample/Sample.kt
@@ -1,7 +1,8 @@
 package com.github.daneko.sample
 
 import arrow.core.Either
-import arrow.core.Try
+import arrow.core.left
+import arrow.core.right
 import arrow.instances.either.monad.binding
 
 data class Hoge(val i: Int)
@@ -19,18 +20,28 @@ fun <A> realworld(f: () -> A): A = Math.random().let {
     else throw RuntimeException("／(^o^)＼")
 }
 
+typealias MyTry<A> = Either<Throwable, A>
+
 interface Sample {
 
-    fun getHoge(i: Int): Try<Hoge> = Try { realworld { Hoge(i) } }
+    fun getHoge(i: Int): MyTry<Hoge> = try {
+        realworld { Hoge(i) }.right()
+    } catch (e: Exception) {
+        e.left()
+    }
 
-    fun getFuga(i: Int): Try<Fuga> = Try { realworld { Fuga(i) } }
+    fun getFuga(i: Int): MyTry<Fuga> = try {
+        realworld { Fuga(i) }.right()
+    } catch (e: Exception) {
+        e.left()
+    }
 
     fun getResult(a: Int, b: Int): Either<MyError, Result> {
 
         return binding {
-            val hoge = getHoge(a).toEither { MyError.HogeError(it) }.bind()
+            val hoge = getHoge(a).mapLeft { MyError.HogeError(it) }.bind()
 
-            val fuga = getFuga(b).toEither { MyError.FugaError(it) }.bind()
+            val fuga = getFuga(b).mapLeft { MyError.FugaError(it) }.bind()
 
             Result(hoge.i + fuga.i)
         }
