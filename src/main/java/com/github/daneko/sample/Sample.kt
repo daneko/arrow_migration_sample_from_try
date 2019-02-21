080aea8c98942b0cf652922b716dfadf5ac1de86
daneko <3a3k.web+github@gmail.com>
2019-02-21 22:11:27 +0900

    using IO and EitherT

--- src/main/java/com/github/daneko/sample/Sample.kt
+++ src/main/java/com/github/daneko/sample/Sample.kt
@@ -1,8 +1,14 @@
 package com.github.daneko.sample
 
-import arrow.core.Either
-import arrow.core.Try
-import arrow.instances.either.monad.binding
+import arrow.core.left
+import arrow.core.right
+import arrow.data.EitherT
+import arrow.effects.ForIO
+import arrow.effects.IO
+import arrow.effects.fix
+import arrow.effects.handleErrorWith
+import arrow.effects.instances.io.monad.monad
+import arrow.instances.eithert.monad.binding
 
 data class Hoge(val i: Int)
 data class Fuga(val i: Int)
@@ -14,23 +20,43 @@ sealed class MyError : Throwable() {
     data class FugaError(override val cause: Throwable) : MyError()
 }
 
+class HandleException : Exception("plz handling")
+
 fun <A> realworld(f: () -> A): A = Math.random().let {
-    if (it > 0.1) f()
-    else throw RuntimeException("／(^o^)＼")
+    when {
+        it > 0.3 -> f()
+        it > 0.01 -> throw HandleException()
+        else -> throw RuntimeException("／(^o^)＼")
+    }
 }
 
 interface Sample {
 
-    fun getHoge(i: Int): Try<Hoge> = Try { realworld { Hoge(i) } }
+    fun getHoge(i: Int): IO<Hoge> = IO { realworld { Hoge(i) } }
 
-    fun getFuga(i: Int): Try<Fuga> = Try { realworld { Fuga(i) } }
+    fun getFuga(i: Int): IO<Fuga> = IO { realworld { Fuga(i) } }
 
-    fun getResult(a: Int, b: Int): Either<MyError, Result> {
+    fun getResult(a: Int, b: Int): EitherT<ForIO, MyError, Result> {
 
-        return binding {
-            val hoge = getHoge(a).toEither { MyError.HogeError(it) }.bind()
+        return binding(IO.monad()) {
 
-            val fuga = getFuga(b).toEither { MyError.FugaError(it) }.bind()
+            val hoge = EitherT(getHoge(a)
+                .map { it.right() }
+                .handleErrorWith {
+                    when (it) {
+                        is HandleException -> IO.just(MyError.HogeError(it).left())
+                        else -> IO.raiseError(it)
+                    }
+                }).bind()
+
+            val fuga = EitherT(getFuga(a)
+                .map { it.right() }
+                .handleErrorWith {
+                    when (it) {
+                        is HandleException -> IO.just(MyError.FugaError(it).left())
+                        else -> IO.raiseError(it)
+                    }
+                }).bind()
 
             Result(hoge.i + fuga.i)
         }
@@ -41,6 +67,8 @@ fun main(args: Array<String>) {
     val sample = object : Sample {}
 
     for (it in 1..10) {
-        println(sample.getResult(it, it * 2))
+        sample.getResult(it, it * 2).value().fix().unsafeRunAsync {
+            println(it)
+        }
     }
 }
