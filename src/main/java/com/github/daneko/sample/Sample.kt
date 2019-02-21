98a1bf9305a12a05888d76f9119745aad9a51b7e
daneko <3a3k.web+github@gmail.com>
2019-02-21 21:39:41 +0900

    use deferredk

--- src/main/java/com/github/daneko/sample/Sample.kt
+++ src/main/java/com/github/daneko/sample/Sample.kt
@@ -1,7 +1,8 @@
 package com.github.daneko.sample
 
 import arrow.core.Either
-import arrow.core.Try
+import arrow.effects.DeferredK
+import arrow.effects.unsafeAttemptSync
 import arrow.instances.either.monad.binding
 
 data class Hoge(val i: Int)
@@ -21,16 +22,18 @@ fun <A> realworld(f: () -> A): A = Math.random().let {
 
 interface Sample {
 
-    fun getHoge(i: Int): Try<Hoge> = Try { realworld { Hoge(i) } }
+    fun getHoge(i: Int): DeferredK<Hoge> = DeferredK { realworld { Hoge(i) } }
 
-    fun getFuga(i: Int): Try<Fuga> = Try { realworld { Fuga(i) } }
+    fun getFuga(i: Int): DeferredK<Fuga> = DeferredK { realworld { Fuga(i) } }
 
     fun getResult(a: Int, b: Int): Either<MyError, Result> {
 
         return binding {
-            val hoge = getHoge(a).toEither { MyError.HogeError(it) }.bind()
 
-            val fuga = getFuga(b).toEither { MyError.FugaError(it) }.bind()
+            // if remove Try, What will becomes of unsafeAttemptSync ?
+            val hoge = getHoge(a).unsafeAttemptSync().toEither { MyError.HogeError(it) }.bind()
+
+            val fuga = getFuga(b).unsafeAttemptSync().toEither { MyError.FugaError(it) }.bind()
 
             Result(hoge.i + fuga.i)
         }
